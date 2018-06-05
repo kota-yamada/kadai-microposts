652f23b7ddbff16d02a31620362eade4af60cde8
kota.yamada <ec2-user@ip-172-31-5-57.us-east-2.compute.internal>
2018-06-05 01:48:20 +0000

    post

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,11 +10,6 @@ class UsersController extends Controller
 {
     public function index()
     {
-        $users = User::all();
-        
-        return view('users.index', [
-            'users' => $users,
-        ]);
         
         $users = User::paginate(10);
         
@@ -26,9 +21,15 @@ class UsersController extends Controller
     public function show($id)
     {
         $user = User::find($id);
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
 
-        return view('users.show', [
+        $data = [
             'user' => $user,
-        ]);
+            'microposts' => $microposts,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.show', $data);
     }
 }
